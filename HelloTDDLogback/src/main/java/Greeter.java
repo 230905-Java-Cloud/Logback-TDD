23ab7e24c210e34220526ae45e06a67d459b83e0
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-10-11 11:16:48 -0400

    three logs with logback

--- HelloTDDLogback/src/main/java/Greeter.java
+++ HelloTDDLogback/src/main/java/Greeter.java
@@ -1,14 +1,30 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Greeter {
 
+    //Instantiate a Logger object for the Greeter class so we can trigger some logs
+    private static final Logger logger = LoggerFactory.getLogger(Greeter.class);
+
     public String genericGreeting(){
+
+        //INFO log - logs pertaining to typical application process
+        logger.info("User got greeted!");
+
         return "Hey There!";
     }
 
     public String personalGreeting(String name){
 
         if(name.equals("")){
+
+            //WARN log - logs that pertain to user error or potential security issues
+            logger.warn("User tried to enter empty String for a name! what were they thinking?");
+
             throw new IllegalArgumentException("Name must not be empty!");
         }
+
+        logger.info("User entered name: " + name);
 
         return "Hello, " + name + "!";
     }
